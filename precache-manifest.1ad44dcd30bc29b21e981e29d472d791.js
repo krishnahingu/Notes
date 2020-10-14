8a7fcd4baf149cbef6db85e96291c528423595f1
krishna hingu <krishnahingu19@gmail.com>
2020-10-15 02:02:19 +0530

    init

---
+++ precache-manifest.1ad44dcd30bc29b21e981e29d472d791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76488178ea6b83a0af3ef3a65148ff3",
+    "url": "/nms/index.html"
+  },
+  {
+    "revision": "ad656fa50eee9fb9430f",
+    "url": "/nms/static/css/main.4ca18780.chunk.css"
+  },
+  {
+    "revision": "5176cd81b38692865089",
+    "url": "/nms/static/js/2.8998e274.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/nms/static/js/2.8998e274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad656fa50eee9fb9430f",
+    "url": "/nms/static/js/main.74d9115b.chunk.js"
+  },
+  {
+    "revision": "756037636e116ab61e14",
+    "url": "/nms/static/js/runtime-main.bef05f16.js"
+  }
+]);
